003db13e6496a8a97a4f84ad5bb2f8e01b3b1aba
Ryan Wang <ryanwang.cs@gmail.com>
2018-09-14 19:34:42 -0400

    Use auto format.

--- statdp/generators.py
+++ statdp/generators.py
@@ -14,7 +14,7 @@ def argument_generator(algorithm, d1, d2, default_kwargs):
     # TODO: implement argument_generator
     arguments = algorithm.__code__.co_varnames[:algorithm.__code__.co_argcount]
     if arguments[1] not in default_kwargs:
-        logger.error('The second argument {0} (privacy budget) is not provided!'.format(arguments[1]))
+        logger.error('The second argument {} (privacy budget) is not provided!'.format(arguments[1]))
         return None
 
     return default_kwargs
