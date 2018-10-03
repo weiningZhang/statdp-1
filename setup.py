7e54bc99272431774be41c4d6fe5be2be16dcd6c
Ryan Wang <ryanwang.cs@gmail.com>
2018-10-03 15:15:09 -0400

    Use plain open since version requirement is python 3.

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup, find_packages
-from codecs import open
 
 # Get the long description from the relevant file
-with open('README.md', encoding='utf-8') as f:
+with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
