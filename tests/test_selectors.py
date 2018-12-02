197299b3656eae697fc6ca94bd4be819e5bf621f
Ryan Wang <ryanwang.cs@gmail.com>
2018-12-01 23:29:55 -0500

    Pass tuples into select_event instead of lists.

--- tests/test_selectors.py
+++ tests/test_selectors.py
@@ -7,8 +7,8 @@ def test_select_event():
     pool = mp.Pool(mp.cpu_count())
     d1 = [0] + [2 for _ in range(4)]
     d2 = [1 for _ in range(5)]
-    _, _, _, event = select_event(noisy_max_v1a, [(d1, d2, {'epsilon': 0.5})], 0.5, 100000, process_pool=pool)
+    _, _, _, event = select_event(noisy_max_v1a, ((d1, d2, {'epsilon': 0.5}),), 0.5, 100000, process_pool=pool)
     assert 0 in event
-    _, _, _, event = select_event(noisy_max_v1b, [(d1, d2, {'epsilon': 0.5})], 0.5, 100000, process_pool=pool)
+    _, _, _, event = select_event(noisy_max_v1b, ((d1, d2, {'epsilon': 0.5}),), 0.5, 100000, process_pool=pool)
     assert 0 in event
 
