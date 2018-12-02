f90861b2e71b1a874b191d18cf8509bd28ad7038
Ryan Wang <ryanwang.cs@gmail.com>
2018-12-02 18:06:02 -0500

    Only expose needed interfaces.

--- statdp/__init__.py
+++ statdp/__init__.py
@@ -1,4 +1,4 @@
 from statdp.hypotest import hypothesis_test
-from statdp.generators import *
-from statdp.selectors import *
+from statdp.generators import generate_arguments, generate_databases
+from statdp.selectors import select_event
 from statdp.core import detect_counterexample
