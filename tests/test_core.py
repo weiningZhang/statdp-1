d80dff237f4aaa390e96a3e7d7821121f26e7448
Ryan Wang <ryanwang.cs@gmail.com>
2018-12-02 18:19:58 -0500

    Add test for noisy max v2a.

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,5 +1,6 @@
 from statdp.core import detect_counterexample
-from statdp.algorithms import noisy_max_v1a
+from statdp.algorithms import noisy_max_v1a, noisy_max_v1b, noisy_max_v2a, noisy_max_v2b,\
+    SVT, iSVT1, iSVT2, iSVT3, iSVT4
 
 
 def test_main():
@@ -16,3 +17,13 @@ def test_main():
                                    databases=(d1, d2), loglevel=logging.DEBUG)
     epsilon, p, *_ = result[0]
     assert epsilon == 0.2 and p <= 0.05
+
+    # test for noisy max v2a
+    result = detect_counterexample(noisy_max_v2a, (0.2, 0.7, 1.5), {'epsilon': 0.7}, loglevel=logging.DEBUG)
+    assert isinstance(result, list) and len(result) == 3
+    epsilon, p, *_ = result[0]
+    assert epsilon == 0.2 and p <= 0.05, 'result {} is not expected.'.format(result[0])
+    epsilon, p, *_ = result[1]
+    assert epsilon == 0.7 and p >= 0.05, 'result {} is not expected.'.format(result[0])
+    epsilon, p, *_ = result[2]
+    assert epsilon == 1.5 and p >= 0.95, 'result {} is not expected.'.format(result[0])
