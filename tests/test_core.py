7f5754fe7490c903c10345aedb24031374657751
Ryan Wang <ryanwang.cs@gmail.com>
2018-12-02 21:25:12 -0500

    Split into different functions.

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,10 +1,10 @@
+import logging
 from statdp.core import detect_counterexample
 from statdp.algorithms import noisy_max_v1a, noisy_max_v1b, noisy_max_v2a, noisy_max_v2b,\
     SVT, iSVT1, iSVT2, iSVT3, iSVT4
 
 
-def test_main():
-    import logging
+def test_noisy_max_v1a():
     result = detect_counterexample(noisy_max_v1a, 0.5, {'epsilon': 0.2}, loglevel=logging.DEBUG)
     assert isinstance(result, list) and len(result) == 1
     epsilon, p, *_ = result[0]
@@ -18,7 +18,8 @@ def test_main():
     epsilon, p, *_ = result[0]
     assert epsilon == 0.2 and p <= 0.05
 
-    # test for noisy max v2a
+
+def test_noisy_max_v2a():
     result = detect_counterexample(noisy_max_v2a, (0.2, 0.7, 1.5), {'epsilon': 0.7}, loglevel=logging.DEBUG)
     assert isinstance(result, list) and len(result) == 3
     epsilon, p, *_ = result[0]
