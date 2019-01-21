7522718fcdcb882357a915b9131d0111ddf988f0
Ryan Wang <ryanwang.cs@gmail.com>
2019-01-20 23:31:20 -0500

    Move run_algorithm in hypotest module.
    Keep only one copy of run_algorithm function. In this way, module selectors will simply call run_algorithm in hypotest module with `event` argument set to None (which will enable auto event search).

--- statdp/selectors.py
+++ statdp/selectors.py
@@ -26,51 +26,16 @@ import inspect
 import numpy as np
 import tqdm
 
-from statdp.hypotest import test_statistics
+from statdp.hypotest import test_statistics, run_algorithm
 
 logger = logging.getLogger(__name__)
 
 
-    np.random.seed()
 def _evaluate_input(input_triplet, algorithm, iterations):
     d1, d2, kwargs = input_triplet
-    result_d1 = np.fromiter((algorithm(d1, **kwargs) for _ in range(iterations)), dtype=np.float64, count=iterations)
-    result_d2 = np.fromiter((algorithm(d2, **kwargs) for _ in range(iterations)), dtype=np.float64, count=iterations)
-
-    if search_space is None:
-        # determine the search space based on the return type
-        # a subset of results to determine return type
-        combined_result = np.concatenate((result_d1, result_d2))
-        unique = np.unique(combined_result)
-
-        # categorical output
-        if len(unique) < iterations * 0.02 * 0.1:
-            search_space = tuple(key for key in unique)
-        else:
-            combined_result.sort()
-            # find the densest 70% range
-            search_range = int(0.7 * len(combined_result))
-            search_max = min(range(search_range, len(combined_result)),
-                             key=lambda x: combined_result[x] - combined_result[x - search_range])
-            search_min = search_max - search_range
-
-            search_space = tuple((-float('inf'), alpha) for alpha in
-                                 np.linspace(combined_result[search_min], combined_result[search_max], num=25))
-
-        logger.debug('search space is set to {0}'.format(search_space))
-
-    input_event_pairs, counts = [], []
-    for event in search_space:
-        cx = np.count_nonzero(result_d1 == event if isinstance(event, (int, float)) else
-                              np.logical_and(result_d1 > event[0], result_d1 < event[1]))
-        cy = np.count_nonzero(result_d2 == event if isinstance(event, (int, float)) else
-                              np.logical_and(result_d2 > event[0], result_d2 < event[1]))
-        input_event_pairs.append((d1, d2, kwargs, event))
-        counts.append((cx, cy) if cx > cy else (cy, cx))
-    return input_event_pairs, counts
+    return run_algorithm(algorithm, d1, d2, kwargs, None, iterations)
 
 
-def select_event(algorithm, input_list, epsilon, iterations=100000, search_space=None, process_pool=None, quiet=False):
 def select_event(algorithm, input_list, epsilon, iterations=100000, process_pool=None, quiet=False):
     """
     :param algorithm: The algorithm to run on
@@ -90,11 +55,11 @@ def select_event(algorithm, input_list, epsilon, iterations=100000, process_pool
     results = process_pool.imap_unordered(partial_evaluate_input, input_list) if process_pool else \
         map(partial_evaluate_input, input_list)
 
-    input_event_pairs, counts = [], []
+    counts, input_event_pairs = [], []
     # flatten the results for all input/event pairs
-    for local_input_event_pair, local_counts in results:
-        input_event_pairs.extend(local_input_event_pair)
+    for local_counts, local_input_event_pair in results:
         counts.extend(local_counts)
+        input_event_pairs.extend(local_input_event_pair)
 
     # calculate p-values based on counts
     threshold = 0.001 * iterations * np.exp(epsilon)
