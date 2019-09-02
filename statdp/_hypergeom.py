ddede69743b7879c7a1de1c7499422f479c2a2c2
Yuxin Wang <ryanwang.cs@gmail.com>
2019-09-02 01:06:22 -0400

    Implement _hypergeom module.
    This module tries to use GSL implementation of hypergeom.cdf when available, and falls back to scipy implementation if failed.

---
+++ statdp/_hypergeom.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import subprocess
+import shutil
+import ctypes
+import logging
+
+from scipy.stats import hypergeom
+
+logger = logging.getLogger(__name__)
+
+# try to import hypergeometric cdf from Gnu Scientific Library (GSL)
+cdf = None
+use_gsl = True
+if shutil.which('gsl-config'):
+    proc = subprocess.run(['gsl-config', '--prefix'], capture_output=True)
+    lib_path = os.path.join(proc.stdout.decode('utf-8').strip(), 'lib')
+    ext = '.dylib' if 'darwin' in sys.platform else ('.so' if 'linux' in sys.platform else '.dll')
+    # load libgslcblas first
+    ctypes.CDLL(os.path.join(lib_path, 'libgslcblas{}'.format(ext)), mode=ctypes.RTLD_GLOBAL)
+    # load libgsl
+    libgsl = ctypes.CDLL(os.path.join(lib_path, 'libgsl{}'.format(ext)))
+    hyper = libgsl.gsl_cdf_hypergeometric_P
+    hyper.restype = ctypes.c_double
+
+    def cdf(k, M, n, N):
+        return float(hyper(ctypes.c_int(k), ctypes.c_int(n), ctypes.c_int(M - n), ctypes.c_int(N)))
+
+# fall back to scipy implementation
+if not cdf:
+    use_gsl = False
+    cdf = hypergeom.cdf
