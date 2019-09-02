68e73c7574e2eb43f03c930913c3a5bf40941a6a
Yuxin Wang <ryanwang.cs@gmail.com>
2019-09-02 12:49:10 -0400

    Remove support for python3.4 in setup.py.

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setup(
         'Topic :: Differential Privacy :: Statistics',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7'
