8d40cb61a56d70280896182a6d23907cc478dcee
Yuxin Wang <ryanwang.cs@gmail.com>
2019-09-08 17:25:59 -0400

    Make hypergeom.cdf detection more robust.

--- statdp/_hypergeom.py
+++ statdp/_hypergeom.py
@@ -9,30 +9,35 @@ from scipy.stats import hypergeom
 
 logger = logging.getLogger(__name__)
 
-# try to import hypergeometric cdf from Gnu Scientific Library (GSL)
 cdf = None
 use_gsl = True
+
+# try to import hypergeometric cdf from Gnu Scientific Library (GSL)
 if shutil.which('gsl-config'):
     proc = subprocess.run(['gsl-config', '--prefix'], stdout=subprocess.PIPE)
+
+    # try to find the library file
     lib_path = os.path.join(proc.stdout.decode('utf-8').strip(), 'lib')
     ext = '.dylib' if 'darwin' in sys.platform else ('.so' if 'linux' in sys.platform else '.dll')
     if not os.path.exists(os.path.join(lib_path, 'libgslcblas{}'.format(ext))):
         lib_path = os.path.join(lib_path, 'x86_64-linux-gnu')
-        if not (os.path.exists(lib_path) and os.path.exists(os.path.join(lib_path, 'libgslcblas{}'.format(ext)))):
-            # libgsl not found, fall back to scipy implementation
-            use_gsl = False
-            cdf = hypergeom.cdf
+        if not os.path.exists(os.path.join(lib_path, 'libgslcblas{}'.format(ext))):
+            # libgsl not found
+            lib_path = None
 
-    # libgsl found
-    if not cdf:
+    if lib_path:
+        dll = ctypes.CDLL if 'darwin' in sys.platform or 'linux' in sys.platform else ctypes.WinDLL
         # load libgslcblas first
-        ctypes.CDLL(os.path.join(lib_path, 'libgslcblas{}'.format(ext)), mode=ctypes.RTLD_GLOBAL)
+        dll(os.path.join(lib_path, 'libgslcblas{}'.format(ext)), mode=ctypes.RTLD_GLOBAL)
         # load libgsl
-        libgsl = ctypes.CDLL(os.path.join(lib_path, 'libgsl{}'.format(ext)))
+        libgsl = dll(os.path.join(lib_path, 'libgsl{}'.format(ext)))
         hyper = libgsl.gsl_cdf_hypergeometric_P
         hyper.restype = ctypes.c_double
 
         def cdf(k, M, n, N):
             return float(hyper(ctypes.c_int(k), ctypes.c_int(n), ctypes.c_int(M - n), ctypes.c_int(N)))
 
-
+# if failed to load GSL, fall back to scipy implementation
+if not cdf:
+    use_gsl = False
+    cdf = hypergeom.cdf
