8dabd54aa186b162e63446e62a95cb7416fffa3d
Yuxin Wang <ryanwang.cs@gmail.com>
2019-10-03 13:17:20 -0400

    Move api functions to __init__ module.
    Instead of having a separate api module.

--- statdp/__init__.py
+++ statdp/__init__.py
@@ -19,7 +19,84 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-from statdp.hypotest import hypothesis_test
+import logging
+import multiprocessing as mp
+
+import tqdm
+
 from statdp.generators import generate_arguments, generate_databases, ALL_DIFFER, ONE_DIFFER
+from statdp.hypotest import hypothesis_test
 from statdp.selectors import select_event
-from statdp.api import detect_counterexample
+from statdp._hypergeom import use_gsl
+
+logger = logging.getLogger(__name__)
+
+
+def detect_counterexample(algorithm, test_epsilon, default_kwargs=None, databases=None, num_input=(5, 10),
+                          event_iterations=100000, detect_iterations=500000, cores=0, sensitivity=ALL_DIFFER,
+                          quiet=False, loglevel=logging.INFO):
+    """
+    :param algorithm: The algorithm to test for.
+    :param test_epsilon: The privacy budget to test for, can either be a number or a tuple/list.
+    :param default_kwargs: The default arguments the algorithm needs except the first Queries argument.
+    :param databases: The databases to run for detection, optional.
+    :param num_input: The length of input to generate, not used if database param is specified.
+    :param event_iterations: The iterations for event selector to run, default is 100000.
+    :param detect_iterations: The iterations for detector to run, default is 500000.
+    :param cores: The cores to utilize, 0 means auto-detection.
+    :param sensitivity: The sensitivity setting, all queries can differ by one or just one query can differ by one.
+    :param quiet: Do not print progress bar or messages, logs are not affected, default is False.
+    :param loglevel: The loglevel for logging package.
+    :return: [(epsilon, p, d1, d2, kwargs, event)] The epsilon-p pairs along with databases/arguments/selected event.
+    """
+    # initialize an empty default kwargs if None is given
+    default_kwargs = default_kwargs if default_kwargs else {}
+
+    logging.basicConfig(level=loglevel)
+    logger.info('Start detection for counterexample on algorithm {} with test epsilon {}'
+                .format(algorithm.__name__, test_epsilon))
+    logger.info('Options -> default_kwargs: {} | databases: {} | cores:{}'.format(default_kwargs, databases, cores))
+
+    # log warnings about gsl installation
+    if use_gsl:
+        logger.info('Found GSL installation, using GSL implementation of hypergeom.cdf for better performance.')
+    else:
+        logger.warning(
+            'Did not find Gnu Scientific Library (GSL) installation, falling back to scipy implementation of '
+            'hypergeom.cdf. Note that GSL provides much faster implementation than scipy which can '
+            'significantly increase detection performance.')
+
+    input_list = []
+    if databases is not None:
+        d1, d2 = databases
+        kwargs = generate_arguments(algorithm, d1, d2, default_kwargs=default_kwargs)
+        input_list = ((d1, d2, kwargs),)
+    else:
+        num_input = (int(num_input), ) if isinstance(num_input, (int, float)) else num_input
+        for num in num_input:
+            input_list.extend(
+                generate_databases(algorithm, num, default_kwargs=default_kwargs, sensitivity=sensitivity))
+
+    result = []
+
+    # convert int/float or iterable into tuple (so that it has length information)
+    test_epsilon = (test_epsilon, ) if isinstance(test_epsilon, (int, float)) else test_epsilon
+
+    pool = mp.Pool(mp.cpu_count()) if cores == 0 else (mp.Pool(cores) if cores != 1 else None)
+    try:
+        for _, epsilon in tqdm.tqdm(enumerate(test_epsilon), total=len(test_epsilon), unit='test', desc='Detection',
+                                    disable=quiet):
+            d1, d2, kwargs, event = select_event(algorithm, input_list, epsilon, event_iterations, quiet=quiet,
+                                                 process_pool=pool)
+            p = hypothesis_test(algorithm, d1, d2, kwargs, event, epsilon, detect_iterations, report_p2=False,
+                                process_pool=pool)
+            result.append((epsilon, float(p), d1, d2, kwargs, event))
+            if not quiet:
+                tqdm.tqdm.write('Epsilon: {} | p-value: {:5.3f} | Event: {}'.format(epsilon, p, event))
+            logger.debug('D1: {} | D2: {} | kwargs: {}'.format(d1, d2, kwargs))
+    finally:
+        if pool:
+            pool.close()
+            pool.join()
+    return result
+
